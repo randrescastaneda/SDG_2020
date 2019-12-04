65f89f90b9cafbad9119be4ac952292447ae2504
Andres Castaneda <acastanedaa@worldbank.org>
2019-12-04 10:42:44 -0500

    add gini to chart

--- R/sdg_10_p10p90p50.R
+++ R/sdg_10_p10p90p50.R
@@ -31,10 +31,18 @@ library("povcalnetR")
 #----------------------------------------------------------
 #   Aux data
 #----------------------------------------------------------
+source("R/_aux_data.R")
 
-load("data/dfc.RData")
 
-df_t <- dfc %>%
+
+#----------------------------------------------------------
+#   filtered dfc data
+#----------------------------------------------------------
+
+lyear <- 2015
+
+
+dfc_1 <- dfc %>%
   filter(status == "OK") %>%
   pivot_wider(
     values_from = threshold,
@@ -42,18 +50,59 @@ df_t <- dfc %>%
     names_prefix = "p",
     id_cols = c(countrycode, year)
   ) %>%
-  filter(year == 2015) %>%
+  filter(year == lyear) %>%
+  # merge regions and country names
+  left_join(cnames) %>%  left_join(rc) %>%
+  arrange(p50)
+
+#----------------------------------------------------------
+#   Gini data
+#----------------------------------------------------------
+
+df_g <- povcalnet(fill_gaps = TRUE) %>%   # Load povcalnet data
+  filter(year == lyear) %>%
+
+  # make we only have one observation per country
+  group_by(countrycode) %>%
+  mutate(n = sequence(n())) %>%
+  filter((n == 1) |
+           (n == 3 & (coveragetype  %in% c("N", "A"))) |
+           (n == 2 & datatype == "consumptioin")) %>%
+
+  # Keep important variables
+  select(countrycode,
+         year,
+         gini,
+         mean,
+         population) %>%
+  arrange(countrycode)
+
+
+dfc_1g <- dfc_1 %>% left_join(df_g) %>%
   mutate(countrycode = factor(countrycode, countrycode))
 
 
-ggplot(data = df_t) +
+#----------------------------------------------------------
+#   charts
+#----------------------------------------------------------
+
+
+adj_scale <- 150
+ggplot(data = dfc_1g) +
   geom_point(aes(x = countrycode,
                 y = p50)) +
+  geom_point(aes(x = countrycode,
+                 y = gini*adj_scale,
+                 color = region)) +
+  scale_y_continuous(sec.axis = sec_axis(~./adj_scale, name = "Gini Index")) +
   geom_errorbar(aes(x = countrycode,
                     ymin = p10,
-                    ymax = p90),
+                    ymax = p90,
+                    color = region),
                 width = 0.5) +
+  theme_classic() +
   theme(
     axis.text.x = element_text(angle = 90,
                                size = 5)
   )
+
