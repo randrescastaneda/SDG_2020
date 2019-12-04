a6dec25e11986da83a6476a2f3aaed929131626b
Andres Castaneda <acastanedaa@worldbank.org>
2019-12-04 08:41:16 -0500

    move file to R folder

--- R/SDG_B40_mean.R
+++ R/SDG_B40_mean.R
@@ -31,37 +31,7 @@ library("povcalnetR")
 #----------------------------------------------------------
 #   Aux data
 #----------------------------------------------------------
-
-load("data/dfc.RData")
-
-#--------- median
-
-dfc50 <- dfc %>%
-  filter(goal == 50) %>%
-  select(countrycode, year,
-         median = threshold)
-
-#--------- names of regions
-
-regions  <- c("ECA", "MNA", "SSA", "LAC", "OHI", "SAS", "EAP")
-
-region_country <- function(x) {
-  cnames <- get_countries(x)
-  df <- tibble(
-    countrycode = cnames,
-    region = x
-  )
-  return(df)
-}
-
-
-rc <- map_dfr(regions, region_country)
-
-#--------- countrynames
-
-cnames <- povcalnet(fill_gaps = TRUE, year = 2015) %>%
-  distinct(countrycode, countryname)
-
+source("data/_aux_data.R")
 
 #----------------------------------------------------------
 # Get data from Povcalnet
