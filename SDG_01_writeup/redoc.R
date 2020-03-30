d4fc34e06e70e1e18a4651b17d3756653d60af8d
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-03-30 12:46:05 -0400

    add writeup folder

---
+++ SDG_01_writeup/redoc.R
@@ -0,0 +1,5 @@
+redoc::dedoc(docx      = "SDG_01_writeup/SDG_01_writeup.docx",
+             to        = "SDG_01_writeup_redoc.Rmd",
+             dir       = "SDG_01_writeup/",
+             overwrite = TRUE)
+
