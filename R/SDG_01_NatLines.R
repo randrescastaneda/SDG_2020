6d17bcb4c3e9c0d899f6cfd24da9f92c915afc4e
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-03-31 15:45:36 -0400

    update national lines code

--- R/SDG_01_NatLines.R
+++ R/SDG_01_NatLines.R
@@ -3,153 +3,88 @@
 #=======================================#
 
 # Libraries
-library(devtools)
-library(caTools)
 
-library(wbstats)
 library(tidyverse)
 library(data.table)
-library(plyr)
-
-library(plotly)
-library(ggplot2)
-library(ggforce)
-library(hrbrthemes)
-library(viridis)
-
 
 #===== Get data an minor clean up =====
 
-data <- data.table(read.csv2("dataNatPovCountryLevel.csv", sep = ","))
+data <- readr::read_csv("data/dataNatPovCountryLevel.csv")
 
 # I'll Stick to comparable data
-dta <- data.table(filter(data, Series == "SI.POV.NAHC"))
-
-# clean year 
-dta$Year = str_extract(dta$Time,"([0-9]+)")
-dta$Time <- NULL
-dta <- dplyr::rename(dta, Value=Data)
-
-dta$Year <-as.numeric(as.character(dta$Year))
-dta$Value <-as.numeric(as.character(dta$Value))
+# clean year
+dta <- data %>%
+  filter(Series == "SI.POV.NAHC") %>%
+  rename(Value = Data,
+         countrycode = Country) %>%
+  mutate(
+    Year = str_extract(Time,"([0-9]+)")
+  ) %>%
+  mutate_at( c("Year", "Value"), as.numeric) %>%
+  select(-Time) %>%
+  setDT()
 
 
 # -- Add Region ID
-# Download country mappings
-countries <- data.table(wbcountries())
-# Set keys to join the data sets
-setkey(dta, Country)
-setkey(countries, iso3c)
-# Add regions to the data set, but remove aggregates
-dta <- countries[dta][ ! region %in% "Aggregates"]
+cr <- read_rds("data/cty_regs_names.rds") %>%
+  setDT()
 
-dta <- dplyr::rename(dta, countrycode=iso3c)
-dta <- dplyr::rename(dta, regioncode=adminID)
+cols  <- c("regioncode", "countryname")
+icols <- paste0("i.", c("region", "countryname"))
+
+dta <- dta[cr,
+          on = c("countrycode"),
+          (cols)  := mget(icols)
+          ]
 
 dta <- dta[complete.cases(dta),]
 
 # drop countries with a sinlge data point
-dta <- dta %>% 
-  group_by(countrycode) %>% 
-  filter( n() > 1)
+dta <- dta %>%
+  group_by(countrycode) %>%
+  filter( n() > 1) %>%
+  ungroup()
 
 clist <- length(unique(dta$countrycode))
 
 #====== Proyection ======
 
-# ------ Calcule yearly growth 
-
-yearly <- ddply(dta,"countrycode",transform,
-                Growth=c(NA,exp(diff(log(Value)))-1))
-
 #------  Using yearly growth
 
 # keeping a minimum of years
-projecty <- yearly %>%
-  group_by(countrycode) %>% 
-  mutate( Iny =  ifelse(abs(Year - 2000) == min(abs(Year - 2000)),Year,0) ) %>% 
-  mutate( Fny =  ifelse(abs(Year - 2015) == min(abs(Year - 2015)),Year,0) ) %>% 
-  mutate( Iny = max(Iny)) %>% 
-  mutate( Fny = max(Fny)) %>% 
-  filter( Iny <= Year & Fny >= Year) %>% 
-  dplyr:: mutate(Period = abs(Year[1] - Year[dplyr::n()]) ) %>% 
-  filter(Period > 5)
+overall <- dta %>%
+  group_by(countrycode) %>%
+  mutate( Iny =  ifelse(abs(Year - 2000) == min(abs(Year - 2000)),Year,0),
+          Fny =  ifelse(abs(Year - 2015) == min(abs(Year - 2015)),Year,0),
+          Iny = max(Iny),
+          Fny = max(Fny)
+          ) %>%
+  filter( Iny <= Year & Fny >= Year) %>%
+  mutate(Period  = Fny - Iny) %>%
+  filter(Period > 5,
+         Year == Iny | Year == Fny) %>%
+  arrange(countrycode, Year) %>%
+  mutate(
+    Growth = Value/lag(Value) -1,            # abs growth
+    Value0 = if_else(Year == Iny, Value,0),  # Value in first year
+    Value0 = max(Value0),                    # max value in first year
+    Term = paste(Iny, Fny, sep = "-"),       # info
+    GAGR = ((Value/Value0)^(1/Period) -1),   # Annualized growth
+    rem_time = if_else(15-Period < 0, 0, 15-Period), # Remaining time to 15-year period
+    project  = Value*(1+GAGR)^rem_time,              # Projection
+    Growthp = ((project - Value0)/Value0) # growth change using proection
+  ) %>%
+  drop_na()
 
 clist2 <- length(unique(projecty$countrycode))
 
-# # calculate Average growth rate
-# projecty <- projecty %>%
-#   group_by(countrycode) %>% 
-#   mutate(Grate = mean(Growth, na.rm=TRUE)) %>%
-#   mutate(percount = Period) %>%
-#   mutate(project = Value)
 
-# keep meaningull vars
-projecty <- projecty %>% 
-  select(countrycode, regioncode, Year, Iny, Fny, Value, Period)
-
-#=== Calculate overall change ====
-
-overall <- projecty %>% 
-  group_by(countrycode) %>% 
-  arrange(Year) %>% 
-  filter( Year == Iny | Year == Fny)
-
-
-overall <- ddply(overall,"countrycode",transform,
-                 Growth=c(NA,exp(diff(log(Value)))-1))
-
-
-# original value as column and "odd" reshape
-overall <- overall %>%
-  group_by(countrycode) %>% 
-  mutate( Value0 = ifelse(Year == Iny, Value,0) ) %>% 
-  mutate( Value0 = max(Value0))
-
-overall <- overall[complete.cases(overall),]
-
-# save term as a string
-
-overall <- overall %>% 
-  mutate(Term = paste(Iny, Fny, sep = "-"))
-
-#==== projection ====
-
-# --- Calculate Compond Average Growth Rate
-overall <- overall %>%
-  mutate(GAGR = ((Value/Value0)^(1/Period) -1) )
-
-
-# # --- Calculate Simple Average Growth Rate
-# overall <- overall %>% 
-#   mutate(GAGR = ( Growth/Period ))
-
-
-# ---- Projection 
-
-overall <- overall %>% mutate( percount = Period ) %>% mutate( project = Value )
- 
-for(i in 1:15){
-  overall <- overall %>%
-    group_by(countrycode) %>%
-    mutate(
-      project = ifelse(percount < 15, project*(1+GAGR),project)
-    ) %>%
-    mutate(
-      percount = percount + 1
-    )
-}
-
-# growth change using proection
-
-overall <- overall %>% 
-  mutate( Growthp = ((project - Value0)/Value0) )
 
 # ==== print (important stuff) ====
 cat(paste(
-  "Code ran:\n", 
+  "Code ran:\n",
   paste0( (clist2/clist)*100," % of original countries kept under rule.\n"),
-  paste("Full countries:", clist),"\n",  
+  paste("Full countries:", clist),"\n",
   paste("Used countries:", clist2))
 )
 
