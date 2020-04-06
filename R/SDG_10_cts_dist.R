8b81c1e177fcdfc9feacdf46e927e9714aaf8cb1
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-04-06 10:48:31 -0400

    update gini

--- R/SDG_10_cts_dist.R
+++ R/SDG_10_cts_dist.R
@@ -26,58 +26,100 @@ library("janitor")
 source("R/utils.R")
 
 
+gini <- function(x, y, w) {
+  y <- deparse(substitute(y))
+  w <- deparse(substitute(w))
+
+  if (length(w) == 0) {
+    w = rep(1, times = length(y))
+  } else {
+    w    <- x[[w]]    # weigth
+  }
+
+  y    <- x[[y]]   # welfare
+  ordy <- order(y)     # order of y
+
+  w    <- w[ordy]      #order weight
+  y    <- y[ordy]      # order welfare
+
+  N    <- sum(w)       # population size
+  Y    <- sum(y*w)     # total welfare
+
+  cw   <- cumsum(w)    # Cumulative weights
+  cy   <- cumsum(y*w)  # Cumulative welfare
+
+  sn   <-  w/N         # share of population
+  my   <- weighted.mean(y, w, na.rm = TRUE)
+
+  i    <- (2*cw - w + 1)/2
+  t2   <- y*(N - i + 1)
+  gini <- 1+(1/N) - (2/(my*N^2))*sum(t2*w)
+  return(gini)
+}
+
+
+
+create_welf <- function(x) {
+  tryCatch(
+    expr = {
+      df <- x %>%
+        filter(regioncode == "XX") %>%
+        arrange(povertyline) %>%
+        mutate(
+          population = population*1e6,
+          popshr     = if_else (row_number() > 1,         # population share
+                                headcount - lag(headcount),
+                                headcount),
+          weight     = population*popshr, # people below threshold
+
+          # cumm welfare
+          welfare   = povertyline*(population*headcount - population*povertygap),
+
+          # cnvert to bin from cumm
+          welfare   =  if_else (row_number() > 1,
+                                welfare - lag(welfare),
+                                welfare),
+
+          # per capita
+          welfare    = welfare/weight
+        )
+    }, # end of expr section
+
+    error = function(e) {
+      df <- tibble(
+        message = e$message
+      )
+    }
+
+  ) # End of trycatch
+
+  return(df)
+}
+
+
+
 #----------------------------------------------------------
 #   prepare data
 #----------------------------------------------------------
 
+# dfr <- rcv_dist(country = "COL", year = 2015,  step = 0.5, pl = 0.5)
 lc <- read_rds("data/recovered_dist.rds")
-dfr <- rcv_dist(country = "COL", year = 2015,  step = 0.5, pl = 0.5)
+lc <- lc %>%
+  map(create_welf)
 
-# df <- dfr %>%
-df <- lc$COL2015 %>%
-  arrange(povertyline) %>%
-  mutate(
-    population = population*1e6,
-    popshr     = if_else (row_number() > 1,         # population share
-                          headcount - lag(headcount),
-                          headcount),
-    weight     = population*popshr, # people below threshold
+# df <- lc$COL2015
 
-    # cumm welfare
-    welfare   = povertyline*(population*headcount - population*povertygap),
-
-    # cnvert to bin from cumm
-    welfare   =  if_else (row_number() > 1,
-                          welfare - lag(welfare),
-                          welfare),
-
-    # per capita
-    welfare    = welfare/weight
-  )
-View(df)
+gt <- lc %>%   # Gini total
+  map_dbl(gini,welfare, weight) %>%
+  tibble(gini = .,
+        id = attr(., "names")
+        )
 
 
-w    <- df$weight    # weigth
-y    <- df$welfare   # welfare
 
-ordy <- order(y)     # order of y
 
-w    <- w[ordy]      #order weight
-y    <- y[ordy]      # order welfare
 
-N    <- sum(w)       # population size
-Y    <- sum(y*w)     # total welfare
 
-cw   <- cumsum(w)    # Cumulative weights
-cy   <- cumsum(y*w)  # Cumulative welfare
-
-sn   <-  w/N         # share of population
-my   <- weighted.mean(y, w, na.rm = TRUE)
-
-i    <- (2*cw - w + 1)/2
-t2   <- y*(N - i + 1)
-gini <- 1+(1/N) - (2/(my*N^2))*sum(t2*w)
-gini
 
 
 #----------------------------------------------------------
