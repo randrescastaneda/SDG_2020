889e1f17c3577f05cc910a9e78d04ae06c52693d
David <davidvargasm1@gmail.com>
2020-04-10 12:51:47 -0400

    Update
    Minor changes on plots and a summary ppt

--- R/SDG_10_non_monetary.R
+++ R/SDG_10_non_monetary.R
@@ -152,7 +152,8 @@ for (ind in indList){
       geom_treemap_subgroup_border() +
       geom_treemap_subgroup_text(place = "centre", grow = T, alpha = 0.5, colour =
                                    "black", fontface = "italic", min.size = 0) +
-      geom_treemap_text(colour = "white", place = "topleft", reflow = T)
+      geom_treemap_text(colour = "white", place = "topleft", reflow = T) +
+      ggtitle(codebook[1,1])
   
   }
   
