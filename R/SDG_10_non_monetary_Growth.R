233b75e32213ce59a4c4c719294f13be6f700e6e
David <davidvargasm1@gmail.com>
2020-04-27 19:26:00 -0400

    Some work

--- R/SDG_10_non_monetary_Growth.R
+++ R/SDG_10_non_monetary_Growth.R
@@ -9,6 +9,7 @@ library(hrbrthemes)
 library(ggplot2)
 library(treemap)
 library(treemapify)
+library(plotly)
 
 # Source get_WDI function
 source("R/get_WDI.R")
@@ -20,7 +21,7 @@ source("R/panel_WDI.R")
 # Parameters
 year0 <- 2005      # Circa which years?
 year1 <- 2015
-max_span <- 4     # how far can an obs be from the reference year?
+max_span <- 3     # how far can an obs be from the reference year?
 min_obs <- 10       # Minimum number of countries to make a plot 
 dirsave <- "SDG_10_imgDV"
 # Which indicators do you need?
@@ -30,10 +31,39 @@ indList <- c("per_sa_allsa.cov_q1_tot",
              "per_sa_allsa.cov_q4_tot",
              "per_sa_allsa.cov_q5_tot")
 
+# get data
 panel_WDI(indList, start=year0, end = year1, maxdist=max_span, cb = TRUE, long = T)
 
 WDI <- WDI %>% 
   arrange(indicatorID, countrycode, Year) %>% 
   group_by(indicatorID, countrycode) %>% 
-  mutate( growth = ((value[2] - value[1])/value[1])*100 )
+  mutate( growth = (((value[2] / value[1])^(1/(usedey-usedsy)))-1) * 100 )  # Annualized growth
+  # mutate( growth = ((value[2] - value[1])/value[1])*100 )              # Overall growth
+
+# Minor Cleanup
+WDI <-  WDI %>% 
+  extract(indicatorID, into = "quartile", regex = "_(q[0-9])_", remove=F)
+
+# # matrix form 
+# WDImat <- WDI %>% 
+#   ungroup() %>% 
+#   filter(Year == year1) %>% 
+#   select(countrycode, value, quartile) %>% 
+#   spread( key = quartile, value) %>% 
+#   remove_rownames() %>% 
+#   column_to_rownames(var = "countrycode")
+# 
+# WDImat <- data.matrix(WDImat)
+# WDImat <- t(WDImat)
+
+# heatmap 
+# a <- heatmap(WDImat, scale = "column", Colv = NA, Rowv = NA)
+
+heat <- ggplot(WDI, aes(countrycode, quartile, fill= growth)) +
+  geom_tile() +
+  scale_fill_distiller(palette = "RdYlBu")
+
+heat
+
+
 
