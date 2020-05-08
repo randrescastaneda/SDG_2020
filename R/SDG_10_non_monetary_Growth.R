51857cfb5cce9584b4f6fd0538ec0f4aaef7b12d
David <davidvargasm1@gmail.com>
2020-05-08 18:09:10 -0400

    Some work

--- R/SDG_10_non_monetary_Growth.R
+++ R/SDG_10_non_monetary_Growth.R
@@ -10,6 +10,7 @@ library(ggplot2)
 library(treemap)
 library(treemapify)
 library(plotly)
+library(RColorBrewer)
 
 # Source get_WDI function
 source("R/get_WDI.R")
@@ -19,51 +20,74 @@ source("R/panel_WDI.R")
 # ================================== code start ==============================
 
 # Parameters
-year0 <- 2005      # Circa which years?
-year1 <- 2015
-max_span <- 3     # how far can an obs be from the reference year?
+year0 <- 2007      # Circa which years?
+year1 <- 2017
+max_span <- 4     # how far can an obs be from the reference year?
 min_obs <- 10       # Minimum number of countries to make a plot 
 dirsave <- "SDG_10_imgDV"
+
+## Change in social protection coverege per quartile ##
+
 # Which indicators do you need?
 indList <- c("per_sa_allsa.cov_q1_tot",
              "per_sa_allsa.cov_q2_tot",
              "per_sa_allsa.cov_q3_tot",
              "per_sa_allsa.cov_q4_tot",
-             "per_sa_allsa.cov_q5_tot")
+             "per_sa_allsa.cov_q5_tot",
+             "per_sa_allsa.ben_q1_tot")
 
 # get data
 panel_WDI(indList, start=year0, end = year1, maxdist=max_span, cb = TRUE, long = T)
 
+# Calculate growth
 WDI <- WDI %>% 
   arrange(indicatorID, countrycode, Year) %>% 
   group_by(indicatorID, countrycode) %>% 
   mutate( growth = (((value[2] / value[1])^(1/(usedey-usedsy)))-1) * 100 )  # Annualized growth
   # mutate( growth = ((value[2] - value[1])/value[1])*100 )              # Overall growth
 
+
+#### heatmap covergare per quartiles ####
+
 # Minor Cleanup
-WDI <-  WDI %>% 
-  extract(indicatorID, into = "quartile", regex = "_(q[0-9])_", remove=F)
+coverind <- c("per_sa_allsa.cov_q1_tot",
+             "per_sa_allsa.cov_q2_tot",
+             "per_sa_allsa.cov_q3_tot",
+             "per_sa_allsa.cov_q4_tot",
+             "per_sa_allsa.cov_q5_tot")
 
-# # matrix form 
-# WDImat <- WDI %>% 
-#   ungroup() %>% 
-#   filter(Year == year1) %>% 
-#   select(countrycode, value, quartile) %>% 
-#   spread( key = quartile, value) %>% 
-#   remove_rownames() %>% 
-#   column_to_rownames(var = "countrycode")
-# 
-# WDImat <- data.matrix(WDImat)
-# WDImat <- t(WDImat)
+coverd <-  WDI %>%
+  filter(indicatorID %in% coverind) %>% 
+  drop_na() %>% 
+  extract(indicatorID, into = "quartile", regex = "_(q[0-9])_", remove=F) %>% 
+  filter(countrycode != "CMR") # Odd value in Cameron 
 
-# heatmap 
-# a <- heatmap(WDImat, scale = "column", Colv = NA, Rowv = NA)
-
-heat <- ggplot(WDI, aes(countrycode, quartile, fill= growth)) +
-  geom_tile() +
-  scale_fill_distiller(palette = "RdYlBu")
-
+heat <- ggplot(coverd,aes(countrycode, quartile) ) +
+  geom_tile(aes(fill= growth)) +
+  scale_fill_distiller(palette = "RdBu",  limits=c(-40, 40), breaks=seq(-40,40,by=10))
+  
 heat
 
+#### compare incidence in the lowest quatile ####
 
+incd <- WDI%>% 
+  filter(indicatorID == "per_sa_allsa.ben_q1_tot") %>% 
+  mutate(Year=paste0("v", Year)) %>% 
+  spread("Year", "value") %>%
+  arrange(v2007) %>% 
+  ungroup() %>% 
+  mutate(id = row_number()) %>% 
+  filter(countrycode != "CMR") # Odd value in Cameron 
+  
+
+ggplot(incd, aes(x = v2007, y = growth, size = v2007, color = incomegroup, label = countrycode)) +
+  geom_point() +
+  geom_text(hjust=1.5, vjust=0.7, size = 3, angle = 0, alpha = 0.8)+
+  xlab("Initial incidence") +
+  ylab("Annual Growth")
+# +
+#   theme(
+#     axis.title.x = element_blank(),
+#     axis.text.x = element_blank()
+#   ) 
 
