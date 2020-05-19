897a35e47aab254bc83f9e8ef7d084052da48892
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-05-19 12:20:43 -0400

    improve hundred people chart

--- R/SDG_10_hundred_people.R
+++ R/SDG_10_hundred_people.R
@@ -25,6 +25,30 @@ library("janitor")
 #----------------------------------------------------------
 source("R/utils.R")
 
+# labels for charts
+addUnits <- function(n) {
+  labels <- ifelse(n < 1000, n,  # less than thousands
+                   ifelse(n < 1e6, paste0(round(n/1e3), 'k'),  # in thousands
+                          ifelse(n < 1e9, paste0(round(n/1e6), 'M'),  # in millions
+                                 ifelse(n < 1e12, paste0(round(n/1e9), 'B'), # in billions
+                                        'too big!')
+                                 )
+                          )
+                   )
+  return(labels)
+}
+
+# Weighted median
+weighted.median <- function(x, w) {
+  w <- w[order(x)]
+  x <- x[order(x)]
+
+  prob <- cumsum(w)/sum(w)
+  ps   <- which(abs(prob - .5) == min(abs(prob - .5)))
+  return(x[ps])
+}
+
+
 #----------------------------------------------------------
 #   Aux data
 #----------------------------------------------------------
@@ -33,40 +57,81 @@ dfc <- read_rds("data/dfc.rds")
 dfr <- read_rds("data/dfr.rds")
 # lc <- read_rds("data/cts_dist.rds")
 
+#----------------------------------------------------------
+#   Set up
+#----------------------------------------------------------
+
 c1 <- read_rds("data/cts_dist.rds")$CRI2015national
 c1$countrycode <- "CRI"
 
 c2 <- read_rds("data/cts_dist.rds")$FIN2015national
 c2$countrycode <- "FIN"
 
-cf <- bind_rows(c2, c1)
-rm(c2, c1)
-
-#----------------------------------------------------------
-#   Set up
-#----------------------------------------------------------
+c3 <- read_rds("data/cts_dist.rds")$COL2015national
+c3$countrycode <- "COL"
 
 
+nq   <- 100 # No. of quantiles
+cf <- bind_rows(c1,c2, c3) %>%
+  arrange(countrycode, welfare) %>%
+  group_by(countrycode) %>%
+  mutate(
+    N  = sum(weight, na.rm = TRUE),
+    cw = cumsum(weight),
+    q  =  floor(cw/((N+1)/nq)) + 1,
+  )
+
+
 #----------------------------------------------------------
-# charts
+# Hundred people
 #----------------------------------------------------------
 
-ggplot(data = filter(cf, welfare < 200),
+# medians
+md <- cf %>%
+  group_by(countrycode) %>%
+  summarise(med = weighted.median(welfare, weight)) %>%
+  arrange(med)
+
+cts <- md %>%
+  pull(countrycode)
+
+
+set.seed(10101)
+plt <- sample(palette, size = 4)
+
+
+ggplot(data = filter(cf, welfare < 100),
        aes(x = welfare,
            weight = weight,
-           color = countrycode)) +
+           fill  = countrycode)) +
   geom_histogram(bins = 100,
-                 fill = "white",
-                 position="dodge",
-                 alpha = 0.5) +
-  theme_classic()
-
-
+                 position="identity",
+                 alpha = .5) +
+  # geom_density(alpha=0.6)    +
+  scale_y_continuous(labels = addUnits) +
+  scale_x_continuous(labels = scales::dollar) +
+  scale_fill_manual(values = palette,
+                     breaks = cts) +
+  geom_vline(data = md,
+             aes(xintercept = med,
+                 color      = countrycode),
+             linetype = "dashed") +
+  scale_color_manual(values = palette,
+                     breaks = cts) +
+  theme_classic() +
+  theme(
+    legend.title = element_blank()
+  ) +
+  labs(y = "Population",
+       x = "Daily income")
 
 
 #----------------------------------------------------------
-#
+# Organize in a different way
 #----------------------------------------------------------
+
+
+
 
 #----------------------------------------------------------
 #
