d50697b7eec9f6a119089bf37ade7bf695511e7a
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-07-06 18:02:10 -0400

    create percentiles again using popshare in povcalnetR

--- R/_deciles_thresholds.R
+++ R/_deciles_thresholds.R
@@ -23,53 +23,50 @@ library("povcalnetR")
 #----------------------------------------------------------
 #   subfunctions
 #----------------------------------------------------------
-wdir <- "C:/Users/wb384996/OneDrive - WBG/WorldBank/DECDG/SDG_2020"
-if (getwd() != wdir) {
-  setwd(wdir)
-}
 
+wran_povcal <- function(popshare, wb = FALSE, ...) {
+
+  if(wb == FALSE) {
+    df <- povcalnet(popshare = popshare, ...)
+    ctr <- "countrycode"
+  } else {
+    df <- povcalnet_wb(popshare = popshare, ...)
+    ctr <- "regioncode"
+  }
+
+
+  df <- df %>%
+    mutate(
+      goal = popshare
+    ) %>%
+    select(!!ctr, year,
+           threshold = povertyline,
+           goal, headcount)
+
+
+  return(df)
+}
 
 #----------------------------------------------------------
 #
 #----------------------------------------------------------
 
-source("R/povcalnet_iterate.R")
-
-
-# year <-
-#   povcalnet(country = "COL",
-#             fill_gaps = TRUE) %>%
-#   select(year) %>% pull
-
-# year <- c(1981, 1990, 1999, 2005, 2010, 2015)
-year <- c(1993, 2002, 2015)
-
-
-# countries <- map_dfr(regions,  function(x)
-#   data.frame(country = get_countries(region_code = x))) %>%
-#   pull
-
 #------- countries
-countries <- get_countries("WLD")
-cdf <- as.list(expand.grid(goal = c(c(1:9) / 10, .9999),
-                           country = countries,
-                           year = year,
-                           tolerance = 4,
-                           stringsAsFactors = FALSE))
+th <- c(1:10) / 10
+dfc <- map_dfr(th, ~wran_povcal(popshare = .x,
+                               fill_gaps = TRUE, # Line up years
+                               server = "AR")
+               )
 
-
-
-dfc <- pmap_dfr(cdf, povcalnet_iterate)
 write_rds(dfc, "data/dfc.rds")
 
 #------ regions
-regions  <- c("ECA", "MNA", "SSA", "LAC", "OHI", "SAS", "EAP", "WLD")
-rdf <- as.list(expand.grid(goal = c(c(1:9) / 10, .9999),
-                           region = regions,
-                           year = year,
-                           stringsAsFactors = FALSE))
 
-dfr <- pmap_dfr(rdf, povcalnet_iterate)
+dfr <- map_dfr(th, ~wran_povcal(popshare = .x,
+                                wb = TRUE,
+                                server = "AR")
+            )
+
 write_rds(dfr, "data/dfr.rds")
 
 
